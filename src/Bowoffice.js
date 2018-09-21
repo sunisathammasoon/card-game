7541264f379561a635a7495e53de7b1d53b3cbe0
unknown <5910110358@psu.ac.th>
2018-09-21 16:39:28 +0700

    Game ปรับรุง - เพิ่มการทายคำศัพท์ 1 คำ

---
+++ src/Bowoffice.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import  BowCharacter from "./BowCharacter";
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attemt: 1,
+        guess: [],
+        completed: false
+    }
+}
+export default class WordCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+    activationHandler = (c) => {
+        let guess = [this.state.guess]+c
+        this.setState({guess})
+        if(guess.length === this.state.chars.length){
+            if(guess === this.state.word){
+                this.setState({guess: [], completed: true})
+            }else{
+                this.setState({guess: [], attemt: this.state.attemt + 1})
+            }
+        }
+    }
+
+    render(){
+        return(
+            <div className="App">
+                {
+                    Array.from(this.state.chars).map(
+                        (c, i) => <BowCharacter value = {c} key = {i} attemt={this.state.attemt}
+                        activationHandler = {this.activationHandler}/>
+                    )
+                }
+                <p>Round : {this.state.attemt}</p>
+                <p>{this.state.completed? "you win" : ""}</p>
+            </div>
+        )
+    }
+}
