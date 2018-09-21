1e619fb6a6efcb33749c90b7f3688e37fa6dc41d
unknown <5910110358@psu.ac.th>
2018-09-21 16:40:33 +0700

    Game ปรับรุง - เพิ่มการแสดงผลของคำศัพท์ใหม่

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WordCard from './WordCard';
+import Bowoffice from './Bowoffice';
 
-const word = "HELLO";
+const word = "5910110358";
+const bow = "Engineering";
 class App extends Component {
   render() {
     return (
     <div className="activecard">
     <WordCard value={word}/>
+    <Bowoffice value={bow}/>
     </div>
     );
    }
