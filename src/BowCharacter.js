325ba91ddfe31e8df8fc9f41e6d3d9e377e9ca4a
unknown <5910110358@psu.ac.th>
2018-09-21 16:39:50 +0700

    Game ปรับรุง - เพิ่มส่วนของ BowCharacter

---
+++ src/BowCharacter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+export default class BowCharacter extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            active : false,
+        }
+    }
+
+    activate = () => {
+        if(!this.state.active){
+            this.props.activationHandler(this.props.value)
+            this.setState({active: true})
+        }
+    }
+    componentDidUpdate(prevProps){
+        if(prevProps.attemt !== this.props.attemt){
+          this.setState({active:false})
+        }
+      }
+    render(){
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return(
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
+    }
+}
